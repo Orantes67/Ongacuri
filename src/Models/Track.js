57c222073b8493a628db82730798194aefd17d39
Hector Orantes <orantesroman39@gmail.com>
2024-03-01 21:25:18 -0600

    Modif Track

--- src/Models/Track.js
+++ src/Models/Track.js
@@ -27,23 +27,37 @@ export class Track {
   getMusic() {
     return this.#Music;
   }
-  cargarCnom() {
-    newnom=[]
-  }
-  cargarImg() {
+  cargarCnom(posicion) {
     let Mapear = new Map([
-        ['../image/music1.jpg'],['tomates'],['cebollas']
-      ]);
-      for (let img of Mapear.keys()) {
-        
-      }
+      ["Junior H - 1004 Kilómetros"],
+      ["Junior H - Cherekon"],
+      ["Junior H - Yo voy amarte"],
+      ["Junior H - Naci para amarte"],
+    ]);
+    return Mapear.get(posicion);
+}
+  cargarImg(posicion) {
+    let Mapear = new Map([
+      ["../image/music1.jpg"],
+      ["../image/music2.jpg"],
+      ["../image/music3.jpg"],
+      ["../image/music4.jpg"],
+    ]);
+    return Mapear.get(posicion)
   }
   cargarMusic() {
-    const musica = "../Audio";
-    newmusic=[]
+    let Mapear = new Map([
+      ["../Audio/musica1.mp3"],
+      ["../Audio/musica2.mp3"],
+      ["../Audio/musica3.mp3"],
+      ["../Audio/musica4.mp3"],
+    ]);
+   
   }
-  cargarDrc(){
-    newdrc=[]
+  cargarDrc() {
+    let Mapear = new Map([["4:42"], ["4:24"], ["4:46"], ["4:32"]]);
+    for (let drc of Mapear.keys()) {
+      return drc;
+    }
   }
-  
 }
